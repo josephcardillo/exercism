248b902119f421ce82673235896772c5a998f536
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2020-12-29 12:22:57 -0500

    two-fer edits

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,23 @@
 def two_fer(name="you"):
     return f"One for {name}, one for me."
+
+# def fav_book(book="I don't like to read."):
+#     if book != "I don't like to read.":
+#         print(f"My favorite book is {book}.")
+#     else:
+#         print(book)
+
+def fav_book(*books):
+    if books:
+        for book in books:
+            print(f"My favorite book is {book}.")
+    else:
+        print("I don't like to read.")
+
+# def fav_book(book):
+#     try:
+#         print(f"My favorite book is {book}.")
+#     except:
+#         print("I don't like to read.")
+#     finally:
+#         print("I don't like to read.")
